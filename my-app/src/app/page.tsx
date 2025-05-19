1a232613d150527e1951fabdb792223de79f0edf
HoiSuen <johnnwong@hotmail.com>
2025-05-19 13:27:09 -0700

    Setup Completed

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    //<main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-base"> Hello World! </h1>
+    //</main>
+    );
+  }
